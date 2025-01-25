8d5d65c201a05dbe1cdafc406dec6384cfaf751e
jose-advh <josearmandodiazhernandez1@gmail.com>
2025-01-25 00:45:36 -0500

    Nav update: social media deleted

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,6 @@
-import { MdCallMissedOutgoing } from "react-icons/md";
-import { FiAlignCenter } from "react-icons/fi";
 import { GoPerson } from "react-icons/go";
 import { FaCode } from "react-icons/fa6";
 import { RiMailAiLine } from "react-icons/ri";
-import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
 
 const pagesItems = [
   {
@@ -26,43 +23,18 @@ const pagesItems = [
   },
 ];
 
-const mediaLinks = [
-  {
-    title: "LinkedIn",
-    url: "https://www.linkedin.com/in/jos%C3%A9-d%C3%ADaz-3074b9316/",
-    icon: FaLinkedin,
-  },
-  {
-    title: "GitHub",
-    url: "https://github.com/jose-advh",
-    icon: FaGithubSquare,
-  },
-];
-
 const Header = () => {
   return (
-    <header className="w-full lg:w-2/4 m-auto bg-purple-900/50 rounded-md shadow-lg shadow-purple-900">
+    <header className="w-95 lg:w-2/4 bg-blue-900/75 m-auto rounded-md shadow-lg shadow-blue-900 fixed left-0 right-0">
       <nav className="flex justify-around py-2 px-6">
         <ul className="flex gap-5 items-center">
           {pagesItems.map((link) => (
             <li key={link.arial}>
-              <a className="hover:text-purple-900" href={link.url}>
-                <span className="hidden lg:block text-white">{link.title}</span>
+              <a href={link.url}>
+                <span className="hidden lg:block text-white hover:text-purple-200">
+                  {link.title}
+                </span>
                 <link.icon className="lg:hidden filter drop-shadow-lg text-[24px] text-white" />
-              </a>
-            </li>
-          ))}
-        </ul>
-        <ul className="flex gap-5">
-          {mediaLinks.map((link) => (
-            <li key={link.arial}>
-              <a
-                className="flex items-center gap-2 hover:text-purple-900"
-                href={link.url}
-              >
-                <span className="hidden lg:block text-white">{link.title}</span>
-                <link.icon className="lg:hidden filter drop-shadow-lg text-[24px] text-white" />
-                <MdCallMissedOutgoing className="hidden lg:block text-white" />
               </a>
             </li>
           ))}
