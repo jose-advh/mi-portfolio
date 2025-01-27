999f778082b53778c1c5a07a1b26d0dcacdaad01
jose-advh <josearmandodiazhernandez1@gmail.com>
2025-01-26 23:47:58 -0500

    Component footer created

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="mt-10">
+      <ul className="flex w-[75%] m-auto justify-between text-blue-200">
+        <li>
+          <a href="#">Lorem Impsun</a>
+        </li>
+        <li>
+          <a href="#">Honor, Lealtad y Respeto</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
